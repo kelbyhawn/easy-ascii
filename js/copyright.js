7fcfc54316f4af89fb5572888fde91a7f9d73d36
Kelby Hawn <kelbyhawn@gmail.com>
2018-01-01 13:08:19 -0800

    added files

---
+++ js/copyright.js
@@ -0,0 +1,5 @@
+var copyright = new Date();
+var year = copyright.getFullYear();
+
+var el = document.getElementById('copyright');
+el.innerHTML = '<p>&copy;' + year + ' ' + 'easyASCII <span style="color:#e2e2e2">//</span>' + ' ' + 'Developed by <a href="http://kelbyhawn.com" target="_blank">Kelby Hawn</a></p>';
