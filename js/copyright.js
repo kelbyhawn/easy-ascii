eb2876260caa457bf69c10c9555e4e22749354b5
Kelby Hawn <kelbyhawn@gmail.com>
2021-02-14 15:13:04 -0800

    removed js inline styles

--- js/copyright.js
+++ js/copyright.js
@@ -1,3 +1,3 @@
 let year = new Date().getFullYear();
 
-document.querySelector('footer').innerHTML = `<p>&copy;${year} easyASCII <span style="color:#e2e2e2">&bull;</span> Made by <a href="https://kelbyhawn.com" target="_blank">Kelby Hawn</a></p>`; 
+document.querySelector('footer').innerHTML = `<p>&copy;${year} easyASCII <span>&bull;</span> Made by <a href="https://kelbyhawn.com" target="_blank">Kelby Hawn</a></p>`; 
