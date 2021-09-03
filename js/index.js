4ca68caeb7a77a0a03af5e8a7837a7e9ba6b4d21
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2021-09-03 16:10:35 -0700

    added events

--- js/index.js
+++ js/index.js
@@ -11,8 +11,16 @@ const clipboard = new ClipboardJS(items);
 
 clipboard.on('success', function (e) {
   console.log(e);
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+
+  e.clearSelection();
 });
 
 clipboard.on('error', function (e) {
   console.log(e);
+  console.error('Action:', e.action);
+  console.error('Trigger:', e.trigger);
 });
+
