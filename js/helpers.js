dc711ca5c35ee0db0ed85d13af23c4551e0188f0
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2021-09-05 18:53:26 -0700

    clean up spacing

--- js/helpers.js
+++ js/helpers.js
@@ -1,18 +1,18 @@
 // Disable hover style on cells that don't have a word-based ascii code
 export const disableHover = () => {
-	document.querySelectorAll('code').forEach(code => {
-		if(code.textContent === '-'){
-			code.classList.add('disabled')
-		}
-	});
+  document.querySelectorAll('code').forEach(code => {
+    if(code.textContent === '-'){
+      code.classList.add('disabled')
+    }
+  });
 }
 
 // Add attributes to li items with their respective text content
 export const addAttributes = () => {
-	document.querySelectorAll('ul.copyable li').forEach(item => {
-		if(item.textContent != '-'){ // don't allow disabled li's to be copied
-			item.classList.add('copyable'); // add class for easier selection
-			item.setAttribute('data-clipboard-text',`${item.textContent}`);
-		}
-	})
+  document.querySelectorAll('ul.copyable li').forEach(item => {
+    if(item.textContent != '-'){ // don't allow disabled li's to be copied
+      item.classList.add('copyable'); // add class for easier selection
+      item.setAttribute('data-clipboard-text',`${item.textContent}`);
+    }
+  })
 }
