4cd21b2a24a4b1e4bf5bddad2496f782549b992e
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-01-26 14:02:48 -0800

    add fun data

--- src/data/fun.js
+++ src/data/fun.js
@@ -1,92 +1,572 @@
 // Fun
 const fun = [
   {
-    name: "dagger",
-    symbol: "†",
-    word: "&dagger;",
+    name: "smiley face",
+    symbol: "☺",
+    word: "-",
     number: "&#8224;",
+  },
+  {
+    name: "filled smiley face",
+    symbol: "☻",
+    word: "-",
+    number: "&#9787;",
+  },
+  {
+    name: "sad face",
+    symbol: "☹",
+    word: "-",
+    number: "&#9785;",
+  },
+  {
+    name: "peace (hand)",
+    symbol: "✌",
+    word: "-",
+    number: "&#9996;",
+  },
+  {
+    name: "peace sign",
+    symbol: "☮",
+    word: "-",
+    number: "&#9774;",
+  },
+  {
+    name: "yin-yang",
+    symbol: "☯",
+    word: "-",
+    number: "&#9775;",
+  },
+  {
+    name: "airplane",
+    symbol: "✈",
+    word: "-",
+    number: "&#9992;",
   },
 ];
 
 // Weather
 const weather = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "sun",
+    symbol: "☀",
+    word: "-",
+    number: "&#9728;",
+  },
+  {
+    name: "cloud",
+    symbol: "☁",
+    word: "-",
+    number: "&#9729;",
+  },
+  {
+    name: "rain",
+    symbol: "☂",
+    word: "-",
+    number: "&#9730;",
+  },
+  {
+    name: "snowflake",
+    symbol: "❄",
+    word: "-",
+    number: "&#10052;",
+  },
+  {
+    name: "snowman",
+    symbol: "☃",
+    word: "-",
+    number: "&#9731;",
   },
 ];
 
 // Celestial
 const celestial = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "waxing crescent moon",
+    symbol: "☽",
+    word: "-",
+    number: "&#9789;",
+  },
+  {
+    name: "waning crescent moon",
+    symbol: "☾",
+    word: "-",
+    number: "&#9790;",
+  },
+  {
+    name: "comet",
+    symbol: "☄",
+    word: "-",
+    number: "&#9732;",
+  },
+  {
+    name: "Mercury",
+    symbol: "☿",
+    word: "-",
+    number: "&#9791;",
+  },
+  {
+    name: "Earth",
+    symbol: "♁",
+    word: "-",
+    number: "&#9793;",
+  },
+  {
+    name: "Venus (female)",
+    symbol: "♀",
+    word: "-",
+    number: "&#9792;",
+  },
+  {
+    name: "Mars (male)",
+    symbol: "♂",
+    word: "-",
+    number: "&#9794;",
   },
 ];
 
 // Cards
 const cards = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "heart (black)",
+    symbol: "♥",
+    word: "&hearts;",
+    number: "&#9829;",
+  },
+  {
+    name: "diamond (black)",
+    symbol: "♦",
+    word: "&diams;",
+    number: "&#9830;",
+  },
+  {
+    name: "spade (black)",
+    symbol: "♠",
+    word: "&spades;",
+    number: "&#9824;",
+  },
+  {
+    name: "club (black)",
+    symbol: "♣",
+    word: "&clubs;",
+    number: "&#9827;",
+  },
+  {
+    name: "heart (red)",
+    symbol: "♡",
+    word: "-",
+    number: "&#9825;",
+  },
+  {
+    name: "diamond (red)",
+    symbol: "♢",
+    word: "-",
+    number: "&#9826;",
+  },
+  {
+    name: "spade (red)",
+    symbol: "♤",
+    word: "-",
+    number: "&#9828;",
+  },
+  {
+    name: "club (red)",
+    symbol: "♧",
+    word: "-",
+    number: "&#9831;",
   },
 ];
 
 // Chess
 const chess = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "King (black)",
+    symbol: "♚",
+    word: "-",
+    number: "&#9818;",
+  },
+  {
+    name: "Queen (black)",
+    symbol: "♛",
+    word: "-",
+    number: "&#9819;",
+  },
+  {
+    name: "Rook (black)",
+    symbol: "♜",
+    word: "-",
+    number: "&#9820;",
+  },
+  {
+    name: "Bishop (black)",
+    symbol: "♝",
+    word: "-",
+    number: "&#9821;",
+  },
+  {
+    name: "Knight (black)",
+    symbol: "♞",
+    word: "-",
+    number: "&#9822;",
+  },
+  {
+    name: "Pawn (black)",
+    symbol: "♟",
+    word: "-",
+    number: "&#9823;",
+  },
+  {
+    name: "King (white)",
+    symbol: "♔",
+    word: "-",
+    number: "&#9812;",
+  },
+  {
+    name: "Queen (white)",
+    symbol: "♕",
+    word: "-",
+    number: "&#9813;",
+  },
+  {
+    name: "Rook (white)",
+    symbol: "♖",
+    word: "-",
+    number: "&#9814;",
+  },
+  {
+    name: "Bishop (white)",
+    symbol: "♗",
+    word: "-",
+    number: "&#9815;",
+  },
+  {
+    name: "Knight (white)",
+    symbol: "♘",
+    word: "-",
+    number: "&#9816;",
+  },
+  {
+    name: "Pawn (white)",
+    symbol: "♙",
+    word: "-",
+    number: "&#9817;",
   },
 ];
 
 // Music
 const music = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "quarter note",
+    symbol: "♩",
+    word: "-",
+    number: "&#9833;",
+  },
+  {
+    name: "eighth note",
+    symbol: "♪",
+    word: "-",
+    number: "&#9834;",
+  },
+  {
+    name: "single bar note",
+    symbol: "♫",
+    word: "-",
+    number: "&#9835;",
+  },
+  {
+    name: "double bar note",
+    symbol: "♬",
+    word: "-",
+    number: "&#9836;",
+  },
+  {
+    name: "flat note",
+    symbol: "♭",
+    word: "-",
+    number: "&#9837;",
+  },
+  {
+    name: "natural note",
+    symbol: "♮",
+    word: "-",
+    number: "&#9838;",
+  },
+  {
+    name: "sharp note",
+    symbol: "♯",
+    word: "-",
+    number: "&#9839;",
   },
 ];
 
 // Checks
 const checks = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "check mark",
+    symbol: "✓",
+    word: "-",
+    number: "&#10003;",
+  },
+  {
+    name: "heavy check mark",
+    symbol: "✔",
+    word: "-",
+    number: "&#10004;",
+  },
+  {
+    name: "X",
+    symbol: "✕",
+    word: "-",
+    number: "&#10005;",
+  },
+  {
+    name: "heavy X",
+    symbol: "✖",
+    word: "-",
+    number: "&#10006;",
+  },
+  {
+    name: "ballot X",
+    symbol: "✗",
+    word: "-",
+    number: "&#10007;",
+  },
+  {
+    name: "heavy ballot X",
+    symbol: "✘",
+    word: "-",
+    number: "&#10008;",
+  },
+  {
+    name: "check box (empty)",
+    symbol: "☐",
+    word: "-",
+    number: "&#9744;",
+  },
+  {
+    name: "check box (checked)",
+    symbol: "☑",
+    word: "-",
+    number: "&#9745;",
+  },
+  {
+    name: "ballot box checked",
+    symbol: "☒",
+    word: "-",
+    number: "&#9746;",
   },
 ];
 
 // Office
 const office = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "telephone (filled)",
+    symbol: "☎",
+    word: "-",
+    number: "&#9742;",
+  },
+  {
+    name: "telephone (outline)",
+    symbol: "☏",
+    word: "-",
+    number: "&#9743;",
+  },
+  {
+    name: "telephone (public)",
+    symbol: "✆",
+    word: "-",
+    number: "&#9990;",
+  },
+  {
+    name: "envelope",
+    symbol: "✉",
+    word: "-",
+    number: "&#9993;",
+  },
+  {
+    name: "care of",
+    symbol: "℅",
+    word: "-",
+    number: "&#8453;",
+  },
+  {
+    name: "sign here",
+    symbol: "✍",
+    word: "-",
+    number: "&#9997;",
+  },
+  {
+    name: "pencil",
+    symbol: "✎",
+    word: "-",
+    number: "&#9998;",
+  },
+  {
+    name: "pencil (horizontal)",
+    symbol: "✏",
+    word: "-",
+    number: "&#9999;",
+  },
+  {
+    name: "cut above",
+    symbol: "✁",
+    word: "-",
+    number: "&#9985;",
+  },
+  {
+    name: "cut here",
+    symbol: "✂",
+    word: "-",
+    number: "&#9986;",
+  },
+  {
+    name: "cut below",
+    symbol: "✃",
+    word: "-",
+    number: "&#9987;",
+  },
+  {
+    name: "scissors",
+    symbol: "✄",
+    word: "-",
+    number: "&#9988;",
   },
 ];
 
 // Misc
 const funMisc = [
   {
-    name: "left single quotation",
-    symbol: "‘",
-    word: "&lsquo;",
-    number: "&#8216;",
+    name: "Caduceus",
+    symbol: "☤",
+    word: "-",
+    number: "&#9764;",
+  },
+  {
+    name: "skull and crossbones",
+    symbol: "☠",
+    word: "-",
+    number: "&#9760;",
+  },
+  {
+    name: "biohazard",
+    symbol: "☣",
+    word: "-",
+    number: "&#9763;",
+  },
+  {
+    name: "radioactive",
+    symbol: "☢",
+    word: "-",
+    number: "&#9762;",
+  },
+  {
+    name: "Latin Cross",
+    symbol: "✝",
+    word: "-",
+    number: "&#10013;",
+  },
+  {
+    name: "Latin Cross 3D",
+    symbol: "✞",
+    word: "-",
+    number: "&#10014;",
+  },
+  {
+    name: "Latin Cross outline",
+    symbol: "✟",
+    word: "-",
+    number: "&#10015;",
+  },
+  {
+    name: "Star of David",
+    symbol: "✡",
+    word: "-",
+    number: "&#10017;",
+  },
+  {
+    name: "Maltese Cross",
+    symbol: "✠",
+    word: "-",
+    number: "&#10016;",
+  },
+  {
+    name: "Ankh",
+    symbol: "☥",
+    word: "-",
+    number: "&#9765;",
+  },
+  {
+    name: "Greek Cross",
+    symbol: "☩",
+    word: "-",
+    number: "&#9769;",
+  },
+  {
+    name: "Star and Crescent",
+    symbol: "☪",
+    word: "-",
+    number: "&#9770;",
+  },
+  {
+    name: "Eastern Christian Cross",
+    symbol: "☦",
+    word: "-",
+    number: "&#9766;",
+  },
+  {
+    name: "Chi Rho Cross",
+    symbol: "☧",
+    word: "-",
+    number: "&#9767;",
+  },
+  {
+    name: "Patriarchal Cross",
+    symbol: "☨",
+    word: "-",
+    number: "&#9768;",
+  },
+  {
+    name: "Farsi symbol",
+    symbol: "☫",
+    word: "-",
+    number: "&#9771;",
+  },
+  {
+    name: "Adi Shakti",
+    symbol: "☬",
+    word: "-",
+    number: "&#9772;",
+  },
+  {
+    name: "Hammer and Sickle",
+    symbol: "☭",
+    word: "-",
+    number: "&#9773;",
+  },
+  {
+    name: "Dharma Wheel",
+    symbol: "☸",
+    word: "-",
+    number: "&#9784;",
   },
 ];
 
 export { fun, weather, celestial, cards, chess, music, checks, office, funMisc };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
