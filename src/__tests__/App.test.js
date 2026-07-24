3d6d796f14e3884a17e0e3052c19b0349f53a869
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2026-07-24 10:55:16 -0700

    update jest testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 beforeAll(() => {
   // jsdom does not implement window.matchMedia
